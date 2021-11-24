7bb19ecba4e336a7e2d2e323392db2d8f91960d8
Ivan Zotov <corvuss90@gmail.com>
2021-11-24 23:21:54 +0300

    feat: parse args

--- app/helpers/parseArgs.ts
+++ app/helpers/parseArgs.ts
@@ -1,3 +1,23 @@
-export const parseArgs = (args: string[]) => {
-  console.log(args.slice(2))
+export type ArgsMap = Map<string, string | boolean>
+
+export const parseArgs = (argv: string[]): ArgsMap => {
+  const args = argv.slice(2)
+
+  return args.reduce((acc, item, idx, arr) => {
+    if (item[0] === '-') {
+      if (idx === arr.length - 1) {
+        acc.set(item.substring(1), true)
+        return acc
+      }
+
+      if (arr[idx + 1] !== '-') {
+        acc.set(item.substring(1), arr[idx + 1])
+        return acc
+      }
+
+      acc.set(item.substring(1), true)
+      return acc
+    }
+    return acc
+  }, new Map() as ArgsMap)
 }
--- app/weather.ts
+++ app/weather.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
+import { parseArgs } from './helpers/parseArgs'
+
 const init = () => {
-  console.log(process.argv)
+  const args = parseArgs(process.argv)
+
+  console.log(args.entries())
 }
 
 init()
